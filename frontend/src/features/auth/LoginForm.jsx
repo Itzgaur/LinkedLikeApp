60ebd230652363bf0e2dd1bd2b2994253c94e8d6
Harsh gaur <harshgaur838@gmail.com>
2024-10-16 12:14:52 +0530

    login form

---
+++ frontend/src/features/auth/LoginForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useLogin } from "./useLogin";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login, isPending } = useLogin();
+
+  function handleLogin(e) {
+    e.preventDefault();
+    login({ username, password });
+  }
+  return (
+    <form onSubmit={handleLogin} className="space-y-4 w-full max-w-md">
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="input input-bordered w-full"
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="input input-bordered w-full"
+        required
+      />
+
+      <button type="submit" className="btn btn-primary w-full">
+        {false ? "Loading.." : "Login"}
+      </button>
+    </form>
+  );
+};
+export default LoginForm;
